d4492ff9d6325aba49f330e84e0aba1719308167
K.Kimura <dotnsf@gmail.com>
2023-06-16 21:03:48 +0900

    direction pad

--- helloworld/helloworld.c
+++ helloworld/helloworld.c
@@ -1,11 +1,14 @@
 #include <gb/gb.h>
+#include <gb/drawing.h>
 #include <stdint.h>
 #include <stdio.h>
 
 void main( void ){
     uint8_t i;
 
-    puts( "Hello World!" );
+    gotogxy( 1, 1 );
+    color( 3, 1, SOLID );
+    gprintf( "Hello World!" );
 
     CRITICAL {
         add_SIO(nowait_int_handler);    // disable waiting VRAM state before return
@@ -13,22 +16,36 @@ void main( void ){
     set_interrupts(SIO_IFLAG);          // disable other interrupts. note: this disables sprite movement
 
     while( 1 ) {
-        i = waitpad( J_A | J_B | J_START | J_SELECT );
+        i = waitpad( J_A | J_B | J_START | J_SELECT | J_UP | J_DOWN | J_LEFT | J_RIGHT );
         waitpadup();
 
+        gotogxy( 1, 3 );
         if( i == J_A ){
             // A Button
-            printf( "A" );
+            gprintf( "A" );
         }else if( i == J_B ){
             // B Button
-            printf( "B" );
+            gprintf( "B" );
         }else if( i == J_START ){
             // START Button
-            printf( "START" );
+            gprintf( "START" );
         }else if( i == J_SELECT ) {
             // SELECT Button
-            printf( "SELECT" );
+            gprintf( "SELECT" );
+        }else if( i == J_UP ) {
+            // UP Button
+            gprintf( "UP" );
+        }else if( i == J_DOWN ) {
+            // DOWN Button
+            gprintf( "DOWN" );
+        }else if( i == J_LEFT ) {
+            // LEFT Button
+            gprintf( "LEFT" );
+        }else if( i == J_RIGHT ) {
+            // RIGHT Button
+            gprintf( "RIGHT" );
         }
+
         /* In case of user cancellation */
         waitpadup();
     }
